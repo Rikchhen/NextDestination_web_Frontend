53bad49c5ae9f454da223379bdff2a10e1b52cc1
Rikchhen <rikchhenlama3@gmail.com>
2026-01-20 15:57:08 +0545

    app/(auth)/schema.ts

---
+++ app/(auth)/schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  phoneNumber: z.string().min(10, "Phone number must be at least 10 digits"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+export const registerSchema = z.object({
+  fullName: z.string().min(2, "Name is required"),
+  phoneNumber: z.string().min(5, "Enter a valid email or phone number"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
