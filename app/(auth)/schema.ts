ecdb1946b08f0d87515fd3537304a33b539efb6d
Rikchhen <rikchhenlama3@gmail.com>
2026-02-02 19:22:30 +0545

    app/(auth)/schema.ts modified

--- app/(auth)/schema.ts
+++ app/(auth)/schema.ts
@@ -4,9 +4,18 @@ export const loginSchema = z.object({
   phoneNumber: z.string().min(10, "Phone number must be at least 10 digits"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
+export type LoginFormData=z.infer<typeof loginSchema>
 
 export const registerSchema = z.object({
   fullName: z.string().min(2, "Name is required"),
-  phoneNumber: z.string().min(5, "Enter a valid email or phone number"),
+  phoneNumber: z.string().min(5, "Enter a phone number"),
+  email: z.email(),
   password: z.string().min(6, "Password must be at least 6 characters"),
-});
+  confirmPassword: z.string().min(6, "Password must be at least 6 characters"),
+}).refine(
+  (v) => v.password === v.confirmPassword, {
+    path: ['confirmPassword'],
+    message: "Passwords do not match"
+  }
+);
+export type RegisterFormData = z.infer<typeof registerSchema>;
